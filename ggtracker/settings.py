bb11bc1e5b10402c32456202322c79f0ce526168
Graylin Kim <graylin.kim@gmail.com>
2012-04-25 11:26:45 -0400

    Make database params fully configurable.

--- ggtracker/settings.py
+++ ggtracker/settings.py
@@ -20,8 +20,8 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'ggtracker',                      # Or path to database file if using sqlite3.
-        'USER': 'postgres',                      # Not used with sqlite3.
+        'NAME': os.environ['POSTGRES_DBNAME'],                      # Or path to database file if using sqlite3.
+        'USER': os.environ['POSTGRES_USER'],                      # Not used with sqlite3.
         'PASSWORD': os.environ['POSTGRES_PWORD'],   # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
