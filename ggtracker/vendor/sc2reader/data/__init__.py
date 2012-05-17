988368556129576f8428eda3736372f2ebb6c18f
Graylin Kim <graylin.kim@gmail.com>
2012-05-17 10:54:10 -0400

    Install from the sc2reader repository using pip.
    Removes the vendor folder and the old sc2reader code around with it.
    pip install -r requirements.txt will now install tracking the s2gs
    branch (at least until we merge it into master).

--- ggtracker/vendor/sc2reader/data/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-
-from sc2reader.data.utils import DataObject
-from sc2reader.data.v110_113 import Data_110_113
-from sc2reader.data.v120_131 import Data_120_131
-from sc2reader.data.v132_136 import Data_132_136
-from sc2reader.data.v140_    import Data_140_
