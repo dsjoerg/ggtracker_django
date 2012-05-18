9789af3e600c37b3ecbf0aa739ed2d677ddc10d6
Graylin Kim <graylin.kim@gmail.com>
2012-05-18 19:12:56 -0400

    Now the django uploader works!

--- ggtracker/replays/replay_persister.py
+++ ggtracker/replays/replay_persister.py
@@ -8,25 +8,26 @@ from cStringIO import StringIO
 # 3rd party
 import boto
 import sc2reader
+from sc2reader.plugins.replay import APMTracker, SelectionTracker
 
 # Local
 from models import *
 #from buildnodes import *
 from django.conf import settings
 
-import replay_processors
-
+from replay_processors import WWPMTracker, TrainingTracker, ActivityTracker,OwnershipTracker, LifeSpanTracker
+from replay_processors import army_values
 
 # Register all the sc2reader plugins; order matters!
-sc2reader.register_plugin('Replay',replay_processors.APMTracker)
-sc2reader.register_plugin('Replay',replay_processors.WWPMTracker)
-sc2reader.register_plugin('Replay',replay_processors.TrainingTracker)
-sc2reader.register_plugin('Replay',sc2reader.plugins.replay.SelectionTracker)
+sc2reader.register_plugin('Replay',APMTracker())
+sc2reader.register_plugin('Replay',SelectionTracker())
+sc2reader.register_plugin('Replay',WWPMTracker(ww_length=48))
+sc2reader.register_plugin('Replay',TrainingTracker())
 # These require Selection Tracking
-sc2reader.register_plugin('Replay',replay_processors.ActivityTracker)
-sc2reader.register_plugin('Replay',replay_processors.OwnershipTracker)
+sc2reader.register_plugin('Replay',ActivityTracker())
+sc2reader.register_plugin('Replay',OwnershipTracker())
 #Also requires Training and Ownership Tracking
-sc2reader.register_plugin('Replay',replay_processors.LifeSpanTracker)
+sc2reader.register_plugin('Replay',LifeSpanTracker())
 
 def armyjs_map(replay):
     """Creates a map of player => json array of army units in this format:
@@ -37,9 +38,9 @@ def armyjs_map(replay):
         player_army = list()
         for unit in player.units:
             unit_type = unit.__class__.__name__.lower()
-            unit_type = re.replace(' \((burrowed|sieged)\)', '', unit_type)
-            if unit_type in replay_processors.army_values:
-                player_army.append(unit_type, unit.birth, unit.death)
+            unit_type = unit_type.replace(' \((burrowed|sieged)\)', '')
+            if unit_type in army_values:
+                player_army.append((unit_type, unit.birth, unit.death))
         army_map[player] = json.dumps(player_army)
     return army_map
 
@@ -53,7 +54,7 @@ class ReplayPersister():
 
       def get_replay_file(self, s3_key):
             replay_file = StringIO()
-            key = boto.s3.Key(self.replaybucket, s3_key)
+            key = boto.s3.key.Key(self.replaybucket, s3_key)
             return key.get_contents_to_file(replay_file)
 
       # this function gets called when uploading through the
@@ -104,32 +105,35 @@ class ReplayPersister():
       # this function gets called when uploading through the
       # django-served page
       def upload_complete(self, filename, stringio):
-            m = hashlib.md5()
-            m.update(stringio.getvalue())
-            hash = m.hexdigest()
-            gameDBs = Game.objects.filter(md5hash__exact=hash)
-            assert gameDBs.count() <= 1
-            if gameDBs.count() == 1:
-                  gameDB = gameDBs[0]
-                  gameDB.delete()
+            md5 = hashlib.md5(stringio.getvalue()).hexdigest()
+            replayDB, created = Replay.objects.get_or_create(md5hash__exact=md5)
 
-            gameDB = Game(md5hash=hash, filename=filename)
+            # If we already had the replay, delete the old game record
+            # and start a new one with the same id!
+            if not created:
+                gameDB = Game.objects.get(replay__id__exact=replayDB.id)
+                subdomain = gameDB.subdomain if gameDB.subdomain else ''
+                oldID = gameDB.id
+                gameDB.delete()
+                gameDB = Game(id=oldID, replay=replayDB, subdomain=subdomain)
+            else:
+                gameDB = Game(replay=replayDB, subdomain='')
 
             replay = sc2reader.load_replay(stringio)
             populateGameFromReplay(replay, gameDB)
+
             #for player in replay.players:
             #    self.buildnodes.populate_build(gameDB, player)
 
+            return True
 
 
 def winning_team(replay):
-    winning_team = 0
+    # If we have a team marked as winning use it
     for team in replay.teams:
-#        print "team", dir(team)
-        if hasattr(team, "result"):
-            if team.result == "Win":
-                winning_team = team.number
-    return winning_team
+        if team.result == "Win":
+            return team.number
+    return 0
 
 def createPlayer(player):
       playerDB = Player(name=player.name)
@@ -141,24 +145,18 @@ def createPlayer(player):
       return playerDB
 
 def didPlayerWin(player):
-      if player.team.result == "Win":
+      if player.result == "Win":
             return True
-      elif player.team.result == "Loss":
+      elif player.result == "Loss":
             return False
       else:
             return None
 
-def getStatArr(player, replay, stat):
-      theArr = [0] * (replay.length.seconds/60 + 1)
-      for minute,statvalue in getattr(player, stat + "_arr").items():
-            theArr[minute] = statvalue
-      return theArr
-
-def getWPMArr(player, replay):
-      theArr = [0] * (replay.length.seconds/60 + 1)
-      for minute,workers in player.wpm_arr.items():
-            theArr[minute] = workers
-      return theArr
+def fillStats(player, replay, stat):
+    stats = [0] * (replay.length.mins + 1)
+    for minute, statvalue in getattr(player, stat).items():
+        stats[minute] = statvalue
+    return stats
 
 def getOrCreatePlayer(player):
       playerDBs = Player.objects.filter(bnet_id__exact=player.uid,
@@ -175,76 +173,75 @@ def getOrCreatePlayer(player):
 MAPHEIGHT = 100
 
 def getOrCreateMap(replay):
-      mapDBs = Map.objects.filter(s2ma_hash__exact=replay.map.hash)
+    mapDB, created = Map.objects.get_or_create(s2ma_hash__exact=replay.map_hash)
 
-      assert mapDBs.count() <= 1
-      if mapDBs.count() == 1:
-            mapDB = mapDBs[0]
-      else:
-            mapDB = Map(name=replay.map.name, s2ma_hash=replay.map.hash, gateway=replay.map.gateway)
-            mapDB.save()
+    if created:
+        mapDB.name = replay.map_name
+        mapDB.gateway = replay.gateway
+        mapDB.save()
 
-            # retrieve s2ma from battlenet, extract image
-            replay.map.load()
-            mapsio = StringIO.StringIO(replay.map.minimap)
-            im = Image.open(mapsio)
-            cropped = im.crop(im.getbbox())
-            cropsize = cropped.size
+        # retrieve s2ma from battlenet, extract image
+        replay.load_map()
+        mapsio = StringIO(replay.map.minimap)
+        im = Image.open(mapsio)
+        cropped = im.crop(im.getbbox())
+        cropsize = cropped.size
 
-            # resize height to MAPHEIGHT, and compute new width that
-            # would preserve aspect ratio
-            newwidth = int(cropsize[0] * (float(MAPHEIGHT) / cropsize[1]))
-            finalsize = (newwidth, MAPHEIGHT)
-            resized = cropped.resize(finalsize, Image.ANTIALIAS)
+        # resize height to MAPHEIGHT, and compute new width that
+        # would preserve aspect ratio
+        newwidth = int(cropsize[0] * (float(MAPHEIGHT) / cropsize[1]))
+        finalsize = (newwidth, MAPHEIGHT)
+        resized = cropped.resize(finalsize, Image.ANTIALIAS)
 
-            # write cropped resized minimap image to a string as a png
-            finalIO = StringIO.StringIO()
-            resized.save(finalIO, "png")
+        # write cropped resized minimap image to a string as a png
+        finalIO = StringIO()
+        resized.save(finalIO, "png")
 
-            # store that in S3
-            bucket = boto.connect_s3(settings.AWS_ACCESS_KEY_ID,
-                                     settings.AWS_SECRET_ACCESS_KEY)\
-                                     .lookup(settings.MINIMAP_BUCKET_NAME)
-            k = boto.s3.Key(bucket)
-            k.key = "%s_%i.png" % (replay.map.hash, MAPHEIGHT)
-            k.set_contents_from_string(finalIO.getvalue())
+        # store that in S3
+        bucket = boto.connect_s3(settings.AWS_ACCESS_KEY_ID,
+                                 settings.AWS_SECRET_ACCESS_KEY)\
+                                 .lookup(settings.MINIMAP_BUCKET_NAME)
+        k = boto.s3.key.Key(bucket)
+        k.key = "%s_%i.png" % (replay.map.hash, MAPHEIGHT)
+        k.set_contents_from_string(finalIO.getvalue())
 
-            # clean up
-            finalIO.close()
+        # clean up
+        finalIO.close()
 
-      return mapDB
+    return mapDB
 
 def populateGameFromReplay(replay, gameDB):
-      mapDB = getOrCreateMap(replay)
-      gameDB.map = mapDB
-      gameDB.release_string = replay.release_string
-      gameDB.game_time = datetime.utcfromtimestamp(replay.unix_timestamp)
-      gameDB.winning_team = winning_team(replay)
-      gameDB.game_type = replay.type
-      gameDB.category = replay.category
-      gameDB.duration_seconds = replay.length.seconds
-      gameDB.save()
+    gameDB.map = getOrCreateMap(replay)
+    gameDB.release_string = replay.release_string
+    gameDB.game_time = datetime.utcfromtimestamp(replay.unix_timestamp)
+    gameDB.winning_team = winning_team(replay)
+    gameDB.game_type = replay.type
+    gameDB.category = replay.category
+    gameDB.duration_seconds = replay.length.seconds
+    gameDB.save()
 
-      player_to_army = armyjs_map(replay)
-      for player in replay.players:
-            playerDB = getOrCreatePlayer(player)
+    player_to_army = armyjs_map(replay)
+    for player in replay.players:
+        playerDB = getOrCreatePlayer(player)
 
-            pigDB = PlayerInGame(game=gameDB, player=playerDB)
-            pigDB.team = player.team.number
-            pigDB.chosen_race = player.pick_race[0]
-            pigDB.race = player.play_race[0]
-            pigDB.win = didPlayerWin(player)
-            pigDB.color = player.color.hex
-            pigDB.pid = player.pid
-            pigDB.armies_by_frame = player_to_army[player]
+        pigDB = PlayerInGame(game=gameDB, player=playerDB)
+        pigDB.team = player.team.number
+        pigDB.chosen_race = player.pick_race[0]
+        pigDB.race = player.play_race[0]
+        pigDB.win = didPlayerWin(player)
+        pigDB.color = player.color.hex
+        pigDB.pid = player.pid
+        pigDB.armies_by_frame = player_to_army[player]
 
-            if hasattr(player, "avg_apm"):
-                  pigDB.apm = player.avg_apm
-            if hasattr(player, "wpm"):
-                  pigDB.wpm = player.wpm
-            if hasattr(player, "wpm_arr"):
-                  pigDB.wpm_by_minute = getWPMArr(player, replay)
-            if hasattr(player, "apm_arr"):
-                  pigDB.apm_by_minute = getStatArr(player, replay, "apm")
+        if hasattr(player, "avg_apm"):
+            pigDB.apm = player.avg_apm
+        if hasattr(player, "wwpm"):
+            pigDB.wpm_by_minute = fillStats(player, replay, 'wwpm')
+        if hasattr(player, "apm"):
+            pigDB.apm_by_minute = fillStats(player, replay, 'apm')
 
-            pigDB.save()
+        # TODO: We were going to depreciate this one
+        if hasattr(player, "wpm"):
+            pigDB.wpm = sum(player.wwpm.values())/float(len(player.wwpm))
+
+    pigDB.save()
--- ggtracker/replays/replay_processors.py
+++ ggtracker/replays/replay_processors.py
@@ -1,4 +1,7 @@
+from collections import defaultdict, deque
 from sc2reader.events import *
+from sc2reader.plugins.utils import plugin
+
 
 # TODO: Once the sc2reader:new_data branch is finished we won't need this.
 army_values = {
@@ -77,62 +80,51 @@ army_values = {
 }
 
 terran_training = set([
-    'Marine','Reaper','Marauder','Ghost',                       #Barracks
-    'Hellion','Siege Tank','Thor',                              #Factory
-    'Medivac','Viking','Banshee','Raven','Battlecruiser'        #Starport
+    'marine','reaper','marauder','ghost',                       #Barracks
+    'hellion','siegetank','thor',                               #Factory
+    'medivac','viking','banshee','raven','battlecruiser'        #Starport
 ])
 
 protoss_training = set([
-    'Zealot','Stalker','Sentry','High Templar','Dark Templar'   #Gateway
-    'Warp Prism','Observer','Colossus','Immortal',              #Robotics Bay
-    'Phoenix','Carrier','Void Ray',                             #Stargate
-    'Mothership','Archon'                                       #Nexus/Other
+    'zealot','stalker','sentry','hightemplar','darktemplar'     #Gateway
+    'warpprism','observer','colossus','immortal',               #Robotics Bay
+    'phoenix','carrier','voidray',                              #Stargate
+    'mothership','archon'                                       #Nexus/Other
 ])
 
 
 zerg_training = set([
-    'Overlord','Queen','Zergling','Baneling','Roach',           #Hatchery
-    'Overseer','Hydralisk','Infestor','Mutalisk','Corruptor',   #Lair
-    'Ultralisk','Broodlord'                                     #Hive
+    'overlord','queen','zergling','baneling','roach',           #Hatchery
+    'overseer','hydralisk','infestor','mutalisk','corruptor',   #Lair
+    'ultralisk','broodlord'                                     #Hive
 ])
 
 # NOTE: Doesn't include worker training right now, we don't use it!
 train_commands = zerg_training | protoss_training | terran_training
 
-
-def APMTracker(replay):
-    """Just the straight up simple non-camera action APM"""
-    efilter = lambda event: event.is_player_action
-    for player in replay.players:
-        player.aps = defaultdict(int)
-        player.apm = defacultdict(int)
-        for event in filter(efilter, player.events):
-            player.aps[event.second] += 1
-            player.apm[event.second/60] += 1
-        player.avg_apm = sum(player.apm.values())/minutes
-
+@plugin
 def WWPMTracker(replay, ww_length, workers=set(["Probe", "Drone", "SCV"])):
     """A worker wave are defined as a set of worker built commands within the
     specified ww_length time window."""
-    efilter = lambda e: isinstance(e, AbilityEvent) and e.ability in workers
+    efilter = lambda e: isinstance(e, AbilityEvent) and e.ability_name in workers
     for player in replay.players:
         player.wwpm = defaultdict(int)
         last_worker_built = -ww_length
         for event in filter(efilter, player.events):
-            if (event.framestamp - last_worker_built) > ww_length:
-                last_worker_built = framestamp
+            if (event.frame - last_worker_built) > ww_length:
+                last_worker_built = event.frame
                 event.player.wwpm[event.second/60] += 1
 
+@plugin
 def ActivityTracker(replay):
     """Unit activity marks the first and last times a unit is explicitly
     interacted with by any of the players and/or observers"""
-    def mark_activity(obj,framestamp):
-        if obj.first_activity == None:
-            obj.first_activity = framestamp
-        obj.last_activity = framestamp
+    def mark_activity(obj,frame):
+        obj.first_activity = frame
+        obj.last_activity = frame
 
     # Initialize all objects
-    for obj in replay.objects:
+    for obj in replay.objects.values():
         obj.first_activity = None
         obj.last_activity=None
 
@@ -142,21 +134,22 @@ def ActivityTracker(replay):
         for event in filter(efilter, replay.events):
             # Mark all currently selected units
             for obj in event.selected:
-                mark_activity(obj, framestamp)
+                mark_activity(obj, event.frame)
 
             #Also mark all previously (just before now) selected units
-            for obj in person.selections[event.framestamp-1][0x0A].objects:
-                mark_activity(obj, framestamp)
+            for obj in person.selection[event.frame-1][0x0A].objects:
+                mark_activity(obj, event.frame)
 
         # The last set of units seleced is alive till the end
-        for obj in person.selections[replay.frames].objects:
+        for obj in person.selection[replay.frames][0x0A].objects:
             mark_activity(obj, replay.frames)
 
     # Mark Ability Target Activity
-    efilter = lambda e: isinstance(e, TargetAbilityEvent)
+    efilter = lambda e: isinstance(e, TargetAbilityEvent) and e.target
     for event in filter(efilter, replay.events):
-        mark_activity(event.target, framestamp)
+        mark_activity(event.target, event.frame)
 
+@plugin
 def OwnershipTracker(replay):
     """The ownership tracker uses a variety of game engine constraints to
     establish definite ownership of units."""
@@ -169,7 +162,8 @@ def OwnershipTracker(replay):
             player.units.add(obj)
 
     # Initialize all objects
-    mark_ownership(replay.objects, None)
+    for unit in replay.objects.values():
+        unit.owner = None
 
     # Only loop over players since no one else can own anything
     for player in replay.players:
@@ -178,40 +172,41 @@ def OwnershipTracker(replay):
         # A player can only select more than one unit at a time when selecting her own units
         efilter = lambda e: isinstance(e, SelectionEvent) or isinstance(e, HotkeyEvent)
         for event in filter(efilter, player.events):
-            selection = player.selections[event.framestamp][0x0A]
+            selection = player.selection[event.frame][0x0A]
             if len(selection.objects) > 1:
                 mark_ownership(selection.objects, player)
 
         # A player can only issue orders to her own units
         efilter = lambda e: isinstance(e, AbilityEvent)
         for event in filter(efilter, player.events):
-            mark_ownership(player.selections[event.framestamp][0x0A], player)
+            mark_ownership(player.selection[event.frame][0x0A].objects, player)
 
-
+@plugin
 def TrainingTracker(replay):
     """Tracks each train command with no regard for spam or the action's
     success. Useful for determining when selected units have been birthed."""
     # TODO: Make an attempt to handle cancels?
-    efilter = lambda e: isinstance(e, AbilityEvent) and e.ability in train_commands
+    efilter = lambda e: hasattr(e,'ability_name')
     for player in replay.players:
         player.train_commands = defaultdict(list)
         for event in filter(efilter, player.events):
-            unit_name = ability_name.lower().replace(' ','')
-            player.train_commands[smooshed_ability].append(event.framestamp)
+            ability_name = event.ability_name.lower().replace(' ','').replace('warpin','')
+            if ability_name in train_commands:
+                print ability_name+" was found"
+                player.train_commands[ability_name].append(event.frame)
 
-
+@plugin
 def LifeSpanTracker(replay):
     """Tracks the birth and death of each unit to the best of it's abilities"""
     # TODO: Birth could be much smarter here by using player training commands
     # TODO: Death is obviously taking a very pessimistic view; most units don't
     #        die immediately after their last selection...
-    for unit in replay.objects:
+    for unit in replay.objects.values():
         unit.birth = unit.first_activity
         unit.death = unit.last_activity
 
     # Make a second pass using training information
     for player in replay.players:
-
         # Make a copy using a reversed deque for performance
         unmatched_train_commands = dict()
         for command, frame_list in player.train_commands.items():
@@ -220,18 +215,19 @@ def LifeSpanTracker(replay):
         # Work through the units from last activity to first activity and match
         # them with the most recent train command. This results in the most
         # conservative guesses on our part.
-        for unit in sorted(player.units, key=lambda u: u.birth, reverse=true):
+        for unit in sorted(player.units, key=lambda u: u.birth, reverse=True):
             try:
                 unit_name = unit.__class__.__name__.lower().replace(' ','')
 
-                # Remove all commands that happened after this point in time
-                # TODO: they were probably spam, we could use that information elsewhere
-                unit_build_times = unmatched_train_commands[unit_name]
-                while(unit_build_times[0] >= unit.first_activity):
-                    unit_build_times.pop_left()
+                if unit_name in army_values:
+                    # Remove all commands that happened after this point in time
+                    # TODO: they were probably spam, we could use that information elsewhere
+                    unit_build_times = unmatched_train_commands[unit_name]
+                    while(unit_build_times[0] >= unit.first_activity):
+                        unit_build_times.popleft()
 
-                # Get the first build time before the unit's first_activity
-                unit.birth = unit_build_times.pop_left()
+                    # Get the first build time before the unit's first_activity
+                    unit.birth = unit_build_times.popleft()
 
             except IndexError as e:
                 # A build time wasn't found for this unit?!?!
